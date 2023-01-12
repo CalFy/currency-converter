0bab43c8eeca46ec69039eddae92c873205cbc02
Nikita <2006nak@gmail.com>
2023-01-12 14:14:36 +0200

    add more currency

--- main.js
+++ main.js
@@ -2,16 +2,46 @@ window.onload = function () {
 
 let dolValue = document.getElementById("input")
 let hrnValue = document.getElementById("change")
-let rates = 36.76
+let firstSelect = document.getElementById("select1")
+let secondSelect = document.getElementById("select2")
+let rates = new Object() 
+rates = {
+    "USD": 36.57, 
+    "EUR": 38.79,
+    "UAH": 1
+}
 
 function exchange() {
-    hrnValue.innerHTML = Math.ceil(((dolValue.value * rates) * 100)/100)
+    let inUah = 0
+    if (firstSelect.value === secondSelect.value) {
+        hrnValue.innerHTML = dolValue.value
+    }else if (firstSelect.value === "USD" && secondSelect.value === "UAH") {
+        hrnValue.innerHTML = Math.ceil(((dolValue.value * rates[firstSelect.value]) * 100)/100)
+    }else if (firstSelect.value === "USD" && secondSelect.value === "EUR") {
+        inUah = dolValue.value * rates[firstSelect.value]
+        hrnValue.innerHTML = Math.ceil(((inUah / rates[secondSelect.value]) * 100)/100  )
+    }else if (firstSelect.value === "EUR" && secondSelect.value === "USD") {
+        inUah = dolValue.value * rates[firstSelect.value]
+        hrnValue.innerHTML = Math.ceil(((inUah / rates[secondSelect.value]) * 100)/100  )
+    }else if (firstSelect.value === "EUR" && secondSelect.value === "UAH") {
+        hrnValue.innerHTML = Math.ceil(((dolValue.value * rates[firstSelect.value]) * 100)/100)
+    }else if (firstSelect.value === "UAH" && secondSelect.value === "USD") {
+        hrnValue.innerHTML = Math.ceil(((dolValue.value / rates[secondSelect.value]) * 100)/100)
+    }else if (firstSelect.value === "UAH" && secondSelect.value === "EUR") {
+        hrnValue.innerHTML = Math.ceil(((dolValue.value / rates[secondSelect.value]) * 100)/100)
+    }
 }
 
 dolValue.oninput = function () {
     exchange()
 }
 
-console.log(dolValue)
+firstSelect.oninput = function () {
+    exchange()
+}
+
+secondSelect.oninput = function () {
+    exchange()
+}
 
 }
